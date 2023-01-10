dd2f7f63bc9d5d45615aa685d0aa8df3c607e403
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-01-10 17:27:05 +0900

    [Silver III] Title: 문자열 집합, Time: 96 ms, Memory: 9816 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#define MAX 10001
+using namespace std;
+
+map<string, bool> s;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	string str;
+	int cnt = 0;
+
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		s.insert(pair<string, bool>(str, true));
+	}
+
+	for (int i = 0; i < M; i++) {
+		cin >> str;
+		if (s[str] == true) {
+			cnt++;
+			continue;
+		}
+	}
+
+	cout << cnt << '\n';
+	return 0;
+}
