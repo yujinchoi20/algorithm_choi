6ccfd1258e96cc227c9405d57cf1928c0232c177
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-01-13 13:26:38 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 176 ms, Memory: 28512 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <vector> 
+#include <string>
+using namespace std;
+
+string str;
+vector<string> v;
+vector<string> re;
+map<string, int> m;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> str;
+		v.push_back(str);
+		m.insert(make_pair(str, i));
+	}
+	for (int i = 0; i < M; i++) {
+		cin >> str;
+		if (str[0] >= 65 && str[0] <= 90) {
+			re.push_back(to_string(m[str]));
+		}
+		else {
+			re.push_back(v[stoi(str) - 1]);
+		}
+	}
+	
+	for (int i = 0; i < re.size(); i++) {
+		cout << re[i] << '\n';
+	}
+
+	return 0;
+}
