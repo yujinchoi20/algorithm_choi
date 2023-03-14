480205230b5270f837504f9069acf014faf20197
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-03-14 16:09:47 +0900

    [Silver III] Title: 수열, Time: 1100 ms, Memory: 23768 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, K;
+	static int arr[];
+	static int sum = 0, maxSum = Integer.MIN_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N, K;
+		int arr[];
+		int sum = 0;
+		int maxSum = Integer.MIN_VALUE;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 0; i <= N - K; i++) {
+			sum = 0;
+			for(int j = i; j < i + K; j++) {
+				sum += arr[j];
+			}
+			
+			maxSum = Math.max(maxSum, sum);
+		}
+		
+		System.out.println(maxSum);
+	}
+}
