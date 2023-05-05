8437af1436199111eca90b935e9477d40ff6fb33
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-06 00:54:48 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        
+        int mode = 0;
+        
+        for(int i = 0; i < code.length(); i++) {
+            if(code.charAt(i) == '1'){
+                mode = 1 - mode;
+            } else if(i % 2 == mode) answer += code.charAt(i);
+        }
+        
+        if(answer.equals("")) answer = "EMPTY";
+        
+        return answer;
+    }
+}
