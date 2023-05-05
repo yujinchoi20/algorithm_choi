422b62eccf45f5981f16ac5c8b7625a51fd57bbe
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-06 02:11:38 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,18 +1,12 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
+        int answer = a + b + c;
         
-        if(a == b && b == c) {
-            answer = answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
-        } else if(a == b && b != c) {
-            answer = (a + b + c) * (a*a + b*b + c*c);
-        } else if(a == c && c != b) {
-            answer = (a + b + c) * (a*a + b*b + c*c);
-        } else if (b == c && c != a) {
-            answer = (a + b + c) * (a*a + b*b + c*c);
-        } else {
-            answer = a + b + c;
-        }
+        if(a==b && b == c) {
+            answer = answer * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        } else if(a == b || b == c || c == a) {
+            answer = answer * (a*a + b*b + c*c);
+        } 
         
         return answer;
     }
