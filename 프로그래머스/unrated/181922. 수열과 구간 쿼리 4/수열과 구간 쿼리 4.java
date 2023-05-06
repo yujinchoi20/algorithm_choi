a36c80ec39eb8fc82f28076dbdb15b758be9a335
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-06 21:00:26 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 2.61 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            for(int j = s; j <= e; j++){
+                 arr[j] = j % k == 0 ? arr[j] + 1 : arr[j];
+            }
+                
+        }
+        return arr;
+    }
+}
