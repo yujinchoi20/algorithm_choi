f27c8e45b117dfc7d029a4c4ee462a884c8aeaca
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-16 22:49:37 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = "";
-        String[] str = my_string.split("");
-        int len = str.length;
-        
-        for(int i = len-n; i < len; i++) {
-            answer += str[i];
-        }
-        
-        return answer;
+        return my_string.substring(my_string.length() - n);
     }
 }
