e3dc2d4068a019be2bd140e69cbfc1fc203f76d4
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-18 01:06:02 +0900

    [unrated] Title: 세로 읽기, Time: 8.73 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -2,9 +2,9 @@ class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
         
-        for(int i = 0; i < my_string.length(); i += m) {
+        for(int i = c-1; i < my_string.length(); i += m) {
             //0, m, 2m , ...
-            answer += my_string.substring(i+c-1, i+c);    
+            answer += my_string.charAt(i);    
         }
         
         return answer;
