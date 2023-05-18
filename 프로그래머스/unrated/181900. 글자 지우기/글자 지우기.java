90980f7ac19413fe2b5c7cefacdcd470150aba20
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-19 02:26:01 +0900

    [unrated] Title: 글자 지우기, Time: 12.39 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -1,22 +1,16 @@
-import java.util.Arrays;
-
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        boolean flag = true;
+        char[] cArr = my_string.toCharArray();
         
-        for(int i = 0; i < my_string.length(); i++) {
-            
-            for(int j = 0; j < indices.length; j++) {
-                if(i == indices[j]) { //지우기
-                    flag = false;
-                    break;
-                } else { //추가하기
-                    flag = true;
-                }
+        for(int idx : indices) {
+            cArr[idx] = ' ';
+        }
+        
+        for(char c : cArr) {
+            if(c != ' ') {
+                answer += c;
             }
-            
-            if(flag) answer += my_string.charAt(i);
         }
         
         return answer;
