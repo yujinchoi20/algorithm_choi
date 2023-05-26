ee8505c64b34ac50ae189d7968a78b063f23952b
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-26 15:28:20 +0900

    [unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,17 +1,8 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
+        myString = myString.toLowerCase();
+        myString = myString.replace("a", "A");
         
-        for(int i = 0; i < myString.length(); i++) {
-            if(myString.substring(i, i+1).equals("a")){
-                answer += "A";
-            } else if(myString.substring(i, i+1).equals("A")) {
-                answer += "A";
-            } else {
-                answer += myString.substring(i, i+1).toLowerCase();
-            }
-        }
-        
-        return answer;
+        return myString;
     }
 }
