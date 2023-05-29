51bf13532e288b426e924ac39811347d8c4d0eb3
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-05-30 00:43:27 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 1.90 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,17 +1,13 @@
 class Solution {
     public int solution(String myString, String pat) {
         String str = "";
+        
         for(int i = 0; i < myString.length(); i++) {
-            if(myString.charAt(i) == 65) {
-                str += "B";
-            } else {
-                str += "A";
-            }
+            if(myString.charAt(i) == 65) str += "B";
+            else str += "A";
         }
         
-        if(str.contains(pat)) {
-            return 1;
-        }
+        if(str.contains(pat)) return 1;
         
         return 0;
     }
