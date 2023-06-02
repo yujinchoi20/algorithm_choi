93df96af999f0ef45be81136098ba69bbab18b91
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-02 11:27:53 +0900

    [unrated] Title: 세 개의 구분자, Time: 41.35 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,27 +1,8 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
-    public ArrayList<String> solution(String myStr) {
-        ArrayList<String> answer = new ArrayList<>();
-        String tmp = "";
-        
-        for(int i = 0; i < myStr.length(); i++) {
-            char c = myStr.charAt(i);
-            if(c == 'a' || 
-               c == 'b' || 
-               c == 'c') {
-                if(tmp != "") {
-                    answer.add(tmp);
-                    tmp = "";
-                } 
-            } else {
-                tmp += c;
-            }
-        }
-        
-        if(tmp != "") answer.add(tmp);
-        if(answer.size() == 0) answer.add("EMPTY");
-        
-        return answer;
+    public String[] solution(String myStr) {
+        String[] arr = Arrays.stream(myStr.split("[abc]+")).filter(str -> !str.isEmpty()).toArray(String[]::new);
+        return arr.length == 0 ? new String[] { "EMPTY" } : arr;
     }
 }
