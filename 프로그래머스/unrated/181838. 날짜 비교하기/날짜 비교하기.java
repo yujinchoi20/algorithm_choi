84ad889cb89af568acb7d043a4d8afbe90e6e986
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-10 22:50:42 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,20 +1,18 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        if(date1[0] == date2[0]) {
-            if(date1[1] == date2[1]) {
-                if(date1[2] >= date2[2]) return 0;
-                else return 1;
-                
-            } else if(date1[1] > date2[1]) {
-                return 0;
-            } else {
-                return 1;
+        int answer = 0;
+        
+        for(int i = 0;  i < 3; i++) {
+            if(date1[i] < date2[i]) {
+                answer = 1;
+                break;
             }
-        } else if(date1[0] > date2[0]) {
-            return 0;
-        } else {
-            return 1;
+            else if(date1[i] > date2[i]){
+                answer = 0;
+                break;
+            }
         }
-
+        
+        return answer;
     }
 }
