f82666ac8231854926e1ea06a546e93b13ce3a4e
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-11 23:02:58 +0900

    [unrated] Title: 그림 확대, Time: 0.09 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
@@ -1,17 +1,27 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
-        int len = picture.length;
-        String[] answer = new String[len*k];
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
         
-        for(int i = 0; i < len*k; i++) {
-            answer[i] = "";
+        for(int i = 0; i < k; i++) {
+            sb1.append("x");
+            sb2.append(".");
         }
         
-        for(int i = 0; i < len*k; i++) {
-            for(int j = 0; j < picture[i/k].length(); j++) {
-                for(int z = 0; z < k; z++) {
-                    answer[i] += picture[i/k].charAt(j);
-                }
+        String x = sb1.toString();
+        String dot = sb2.toString();
+        
+        int len = picture.length;
+        String[] answer = new String[len * k];
+        
+        int idx = 0;
+        for(int i = 0; i < len; i++) {
+            String tmp;
+            tmp = picture[i].replace(".", dot);
+            tmp = tmp.replace("x", x);
+
+            for(int j = 0; j < k; j++) {
+                answer[idx++] = tmp;
             }
         }
         
