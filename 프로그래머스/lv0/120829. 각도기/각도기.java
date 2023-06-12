07b5e1598452602e62457aa654f95f39fb65e50b
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-12 22:00:49 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,12 +1,5 @@
 class Solution {
-    public int solution(int angle) {
-        int answer = 0;
-        
-        if(angle < 90) answer = 1;
-        else if(angle == 90) answer = 2;
-        else if(angle > 90 && angle < 180) answer = 3;
-        else answer = 4;
-        
-        return answer;
+    public int solution(int angle) {        
+        return angle < 90 ? 1 : (angle == 90 ? 2 : (angle > 90 && angle < 180 ? 3 : 4));
     }
 }
