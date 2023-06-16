d0906132ace661ef2c6cdde2caf6304283d70af0
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-17 01:39:35 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.95 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,12 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.toLowerCase();
-        String[] arr = answer.split("");
-        
+        String[] arr = my_string.toLowerCase().split("");
         Arrays.sort(arr);
-        answer = "";
         
+        String answer = "";
         for(int i = 0; i < arr.length; i++) {
             answer += arr[i];    
         }
