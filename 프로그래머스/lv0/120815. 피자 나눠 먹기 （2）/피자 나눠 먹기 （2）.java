a71cbec6b56c22ad1718eb85d21d7393327d4e99
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-17 00:30:51 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 1;
         
-        if(n % 6 == 0) {
-            answer = n / 6;
-        } else { 
-            for(int i = 1; i < 100; i++) {
-                if(n * i % 6 == 0) {
-                    answer = (n * i) / 6;
-                    break;
-                }
+        while(true) {
+            if(6 * answer % n == 0){
+                break;
             }
+            answer++;
         }
         
         return answer;
