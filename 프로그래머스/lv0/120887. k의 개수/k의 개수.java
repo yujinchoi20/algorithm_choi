9ff2237a58eb112632ac5ea626080ab48a5587e0
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-20 15:49:39 +0900

    [level 0] Title: k의 개수, Time: 374.54 ms, Memory: 382 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,19 +1,10 @@
 class Solution {
     public int solution(int a, int b, int k) {
-        int answer = 0;
         String str = "";
-        
         for(int i = a; i <= b; i++) {
             str += i+"";
         }      
-    
-        String[] arr = str.split("");
-        
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i].equals(k+""))
-                answer++;
-        }
-        
-        return answer;
+         
+        return str.length() - str.replaceAll(k+"", "").length();
     }
 }
