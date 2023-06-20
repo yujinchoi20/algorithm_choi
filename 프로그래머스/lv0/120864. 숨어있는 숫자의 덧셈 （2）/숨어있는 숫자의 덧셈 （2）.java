cffb1b211b9be152d749f6fb8a4fb93cc91e78d3
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-06-21 02:07:41 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 2.73 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,22 +1,16 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        my_string = my_string.toLowerCase();
+        my_string = my_string.replaceAll("[a-zA-Z]", " ");
         
-        for(int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if(c >= 'a' && c <= 'z') {
-                my_string = my_string.replaceAll(my_string.substring(i, i+1), " ");
-            }
-        }
-        
-        my_string = my_string.replaceAll("\\s+", " ");
         my_string = "0" + my_string;
-        String[] arr = my_string.split(" ");
+        my_string = my_string.replaceAll("\\s+", " ");
+        String[] arr = my_string.split("\\s");
         
         for(int i = 0; i < arr.length; i++) {
             answer += Integer.parseInt(arr[i]);
         }
+        
         return answer;
     }
 }
