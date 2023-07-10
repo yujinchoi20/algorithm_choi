869e3c7ca3a376381906fb263732d77704ba1dfa
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-07-10 12:26:57 +0900

    [level 1] Title: 직사각형 별찍기, Time: 180.55 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -6,11 +6,14 @@ class Solution {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < b; i++) {
             for(int j = 0; j < a; j++) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        
+        System.out.println(sb);
     }
 }
