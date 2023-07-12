dc12d88a11b7e899138aff5e1e2529edd31121ae
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-07-12 23:10:01 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.35 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+        int idx = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i].equals(" ")) {
+                idx = 1;
+            }
+            
+            answer += (idx % 2 == 0 ? arr[i].toUpperCase() : arr[i].toLowerCase());
+            idx++;
+        }
+        
+        
+        return answer;
+    }
+}
