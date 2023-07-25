15440c8cf99256b23cf6ffbd39725653da5fdc0f
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-07-26 02:14:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.45 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -4,27 +4,7 @@ class Solution {
         String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
         for(int i = 0; i < numbers.length; i++) {
-            if(numbers[i].equals("zero") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "0");
-            } else if(numbers[i].equals("one") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "1");
-            } else if(numbers[i].equals("two") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "2");
-            } else if(numbers[i].equals("three") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "3");
-            } else if(numbers[i].equals("four") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "4");
-            } else if(numbers[i].equals("five") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "5");
-            } else if(numbers[i].equals("six") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "6");
-            } else if(numbers[i].equals("seven") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "7");
-            } else if(numbers[i].equals("eight") && s.contains(numbers[i])) {
-                s = s.replaceAll(numbers[i], "8");
-            } else {
-                s = s.replaceAll(numbers[i], "9");
-            }
+            s = s.replaceAll(numbers[i], Integer.toString(i));
         }
         
         answer = Integer.parseInt(s);
