a27a763e9dc029a42074bae5dd2127a3202367a1
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-07-25 15:49:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.59 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,51 +1,15 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        String[][] check = new String[n][n];
-        
-        for(int i = 0; i < arr1.length; i++) {
-            String s1 = Integer.toBinaryString(arr1[i]).toString();
-            String s2 = Integer.toBinaryString(arr2[i]).toString();
-            
-            if(s1.length() < n) {
-                int l = s1.length();
-                for(int j = 0; j < n-l; j++) {
-                    s1 = "0" + s1;
-                }
-            }
-            
-            if(s2.length() < n) {
-                int l = s2.length();
-                
-                for(int j = 0; j < n-l; j++) {
-                    s2 = "0" + s2;
-                }
-            }
-            
-            for(int j = 0; j < n; j++) {
-                if(s1.charAt(j) != s2.charAt(j)){
-                    check[i][j] = "1";
-                } else if(s1.charAt(j) == '1') {
-                    check[i][j] = "1";
-                } else {
-                    check[i][j] = "0";
-                }
-            }
-        }
         
         for(int i = 0; i < n; i++) {
-            String s = "";
+            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
             
-            for(int j = 0; j < n; j++) {
-                if(check[i][j].equals("1")){
-                    s += "#";
-                } else {
-                    s += " ";
-                }
-            }
-            
-            answer[i] = s;
+            answer[i] = String.format("%"+n+"s", s);
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
         }
+        
         return answer;
     }
 }
