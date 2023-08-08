73955c6f60d7610834d5783b15af0cc06fb1ab31
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-08-08 15:19:39 +0900

    [unrated] Title: 과일 장수, Time: 0.62 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,29 +1,14 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        int[] arr = reverse(score);
-        int len = arr.length;
-        int idx = 0;
-        
-        while(len >= m) {
-            answer += arr[idx + m - 1] * m;
-            idx += m;
-            len -= m;
+        Arrays.sort(score);
+        //1 1 1 2 2 3 3 
+        for(int i = score.length - m; i >= 0; i -= m) {
+            answer += score[i] * m;
         }
         
         return answer;
-    }
-    
-    public int[] reverse(int[] arr){
-        int[] reverseArr = new int[arr.length];
-        Arrays.sort(arr);
-        
-        for(int i = arr.length-1, j = 0; i >= 0; i--, j++) {
-            reverseArr[j] = arr[i];
-        }
-        
-        return reverseArr;
     }
 }
