6511de9a0f5776f7d2a95c7adf6e2adcfc0bbbd7
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-08-08 15:47:47 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i = 0; i < nums.length-2; i++) {
+            for(int j = i+1; j < nums.length-1; j++) {
+                for(int k = j+1; k < nums.length; k++) {
+                    int s = nums[i] + nums[j] + nums[k];
+                    if(prime(s)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    //소수판별
+    public boolean prime(int num) {
+        if(num == 1) return false;
+        
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
