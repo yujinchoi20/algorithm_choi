fafd54b3ba766f4796b637641ac3d55c1f117da6
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-08-10 16:02:58 +0900

    [unrated] Title: 덧칠하기, Time: 1.04 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,24 +1,12 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int[] nArr = new int[n+1]; //벽 상태
+        int rollerPoint = 0;
         
-        for(int i = 0; i < section.length; i++) {
-            nArr[section[i]] = 1;
-        }
-        
-        for(int i = 1; i <= n; i++) {
-            if(nArr[i] == 1) { //칠해야 하는 곳
-                for(int j = 0; j < m; j++) {
-                    if(i+j <= n) {
-                        nArr[i+j] = 0; //칠하기 
-                    }
-                    else{
-                        break;
-                    } 
-                }
-                
-                answer++; //롤링 횟수
+        for(int i : section) {
+            if(rollerPoint <= i) {
+                rollerPoint = i + m;
+                answer++;
             }
         }
         
