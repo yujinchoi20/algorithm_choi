4e65a60ce894a5e590f23e378ed3464429cb27de
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-09-01 02:04:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.30 ms, Memory: 95.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        for(String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for(String p : participant) {
+            if(map.get(p) != 0) {
+                answer = p;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
