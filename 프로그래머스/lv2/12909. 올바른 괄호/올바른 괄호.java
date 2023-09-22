6e77a876b4ad5c7b4967534840c265def30a396b
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-09-22 15:20:34 +0900

    [level 2] Title: 올바른 괄호, Time: 11.95 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int check = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') check++;
+            else check--;
+            
+            if(check < 0) 
+                return false;
+        }
+
+        if(check == 0 || s.length() == 0) 
+            return true;
+        else 
+            return false;
+    }
+}
