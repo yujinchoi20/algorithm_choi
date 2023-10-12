c2a765f75769b941f3fec040600a6b8514fb0fd4
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-10-12 13:04:31 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,28 +1,53 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int cnt = 0; 
+        int cnt = 0;
+        HashMap<String, Integer> map = new HashMap<>();
         
         for(int i = 0; i < want.length; i++) {
+            map.put(want[i], number[i]);
             cnt += number[i];
         }
         
         for(int i = 0; i <= discount.length-cnt; i++) {
-            String str = "";
+            HashMap<String, Integer> temp = new HashMap<>();
+
+            for(String key : map.keySet()) {
+                temp.put(key,map.get(key));
+            }
+                
+            
+            boolean flag = true;
             
             for(int j = i; j < i+cnt; j++) {
-                str += discount[j];
+                String str = discount[j];
+                
+                if(!temp.containsKey(str)){ //원하는 물건이 없을 경우
+                    flag = false;
+                    break;
+                }
+                
+                int value = temp.get(str);
+                if(value < 1) { //원하는 물건을 모두 구매한 경우
+                    flag = false;
+                    break;
+                }
+                
+                temp.put(str, value-1); 
             }
             
-            for(int j = 0; j < want.length; j++) {
-                for(int k = 0; k < number[j]; k++) {
-                    str = str.replaceFirst(want[j], "");
+            if(flag) { //만약 사고자하는 물건을 다 구매하지 못했을 경우
+                for(String key : temp.keySet()) {
+                    if(temp.get(key) != 0) {
+                        flag = false;
+                        break;
+                    }
                 }
             }
             
-            if(str.equals("")) {
+            if(flag) {
                 answer++;
             }
         }
