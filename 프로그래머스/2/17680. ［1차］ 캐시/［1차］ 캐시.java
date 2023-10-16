5059cbd5b2103640bcdae3b826368a4531e3b355
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2023-10-16 23:59:29 +0900

    [level 2] Title: [1차] 캐시, Time: 3.40 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(int i = 0; i < cities.length; i++) {
+            cities[i] = cities[i].toLowerCase();
+        }
+        
+        if(cacheSize == 0) {
+            answer = 5 * cities.length;
+            return answer;
+        }
+        
+        //가장 오랫동안 참조되지 않은 페이지를 교체 -> LRU
+        for(int i = 0; i < cities.length; i++) {
+            if(list.size() < cacheSize) { //빈 캐시가 있음
+                if(!list.contains(cities[i])) { //miss
+                    list.add(cities[i]);
+                    answer += 5;
+                } else { //hit
+                    int idx = list.indexOf(cities[i]);
+                    list.remove(idx); //맨 앞 삭제
+                    list.add(cities[i]); //맨 뒤 삽입
+                    answer += 1;
+                }
+            } else { //빈 캐시 없음
+                if(!list.contains(cities[i])) { //miss
+                    list.remove(0);
+                    list.add(cities[i]);
+                    answer += 5;
+                } else { //hit
+                    int idx = list.indexOf(cities[i]);
+                    list.remove(idx);
+                    list.add(cities[i]);
+                    answer += 1;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
