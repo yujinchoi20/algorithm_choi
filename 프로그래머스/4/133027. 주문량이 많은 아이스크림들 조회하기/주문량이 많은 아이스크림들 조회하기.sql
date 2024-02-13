dee0bce985c5c8a504df16bb13e9fabf0d519488
Yujin Choi <105353163+yujinchoi20@users.noreply.github.com>
2024-02-14 00:52:22 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
-select flavor
-from (select * from first_half union select * from july) total
-group by total.flavor
-order by sum(total_order) desc
-limit 3;
+SELECT FLAVOR FROM (SELECT * FROM FIRST_HALF UNION SELECT * FROM JULY) RESULT
+GROUP BY RESULT.FLAVOR
+ORDER BY SUM(TOTAL_ORDER) DESC
+LIMIT 3;
